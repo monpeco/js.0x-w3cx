94a83b394dcada3b6f99dddb12009eeafbc01f85
monpeco <monpeco@gmail.com>
2017-06-04 18:31:43 +0000

    Create geolocation example, ask the current location and display the response

--- intro-js/config/routes.rb
+++ intro-js/config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'static/change_title'
 
   get 'static/function_plot'
+  
+  get 'static/geolocation'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
