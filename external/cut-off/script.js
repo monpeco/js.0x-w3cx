22d3fd0020a320ff1b379ea2bc322d395ef60728
monpeco <monpeco@gmail.com>
2017-07-17 14:26:30 +0000

    Create cutt-off index page with basic style

---
+++ external/cut-off/script.js
@@ -0,0 +1,11 @@
+function init(){
+	console.log("Init.");
+}
+
+function clickSo(){
+	console.log("clickSo 1");
+	var string = document.querySelector("#divSo");
+	console.log(string);
+	console.log("clickSo 2");
+
+}
